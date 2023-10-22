07e2b9fc395eafcf585a51cb36c45436127a3831
alexmb356 <alexmb356@hotmail.com>
2023-10-21 23:12:32 -0300

    se modifica archivo php

--- enviar.php
+++ enviar.php
@@ -1,7 +1,8 @@
 <?php
 $nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
 $mail = $_POST['email'];
-$empresa = $_POST['mensaje'];
+$mensaje = $_POST["mensaje"];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -13,8 +14,8 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'alexmb356@hotmail.com';
-$asunto = 'Mensaje de mi sitio web';
+$para = 'alexmb356@gmail.com';
+$asunto = "Consulta de $nombre $apellido";
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
